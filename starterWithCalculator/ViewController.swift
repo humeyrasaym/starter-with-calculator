bfff0f6e9dedc125cd197a828f4fd90ece150cce
Humeyra Sayim <humeyrasaym@gmail.com>
2025-11-20 16:26:48 +0300

    calculator has been done

--- starterWithCalculator/ViewController.swift
+++ starterWithCalculator/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // UI Elements
+    @IBOutlet weak var firstNumber: UITextField!
+    @IBOutlet weak var secondNumber: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    // Variables
+    var result = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resultLabel.text = "Result"
     }
 
+    @IBAction func sumClicked(_ sender: Any) {
+        calculate(operation: +)
+    }
+    
+    @IBAction func minusClicked(_ sender: Any) {
+        calculate(operation: -)
+    }
+    @IBAction func multiplyClicked(_ sender: Any) {
+        calculate(operation: *)
+    }
+    @IBAction func divideClicked(_ sender: Any) {
+        calculate(operation: /)
+    }
 
+    // +, -, *, / gibi aksiyonlar iki int degeri alip int deger donduren fonksiyonlar oldugu icin asagidaki tanimlamayi yaptim
+    func calculate(operation: (Int, Int) -> Int) {
+        if let first = Int(firstNumber.text ?? "") {
+            if let second = Int(secondNumber.text ?? "") {
+                result = operation(first, second)
+                resultLabel.text = String(result)
+            }
+        }
+    }
 }
 
